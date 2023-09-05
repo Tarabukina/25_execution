3c4af7a877a2ec0aa13ca3760c08c26e8959fb99
Olga Tarabukina <223954@rambler.ru>
2023-09-05 21:24:08 +0900

    add utils operations

---
+++ tests/tests_utils.py
@@ -0,0 +1,24 @@
+from scr import utils
+import pytest
+
+def test_load_json():
+
+    expected_data = [
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+        }
+    ]
+
+    assert func.load_json('operations_1.json') == expected_data
